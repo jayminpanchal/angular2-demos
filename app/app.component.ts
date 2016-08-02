303324958093ce14bada75950b3e90e5f5be0bd8
Jaymin Panchal <jayminpanchal10@gmail.com>
2016-08-02 21:44:07 +0530

    Initial Commit

---
+++ app/app.component.ts
@@ -0,0 +1,6 @@
+    import { Component } from '@angular/core';
+    @Component({
+      selector: 'my-app',
+      template: '<h1>Hello from Jaymin</h1><p>Angular 2 using TS(TypeScript)</p>'
+    })
+    export class AppComponent { }
