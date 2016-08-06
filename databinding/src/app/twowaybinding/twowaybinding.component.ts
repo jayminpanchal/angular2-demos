d38424a4252f793a835bfdd870f55628aaf300bb
Jaymin <jayminpanchal10@gmail.com>
2016-08-07 00:36:17 +0530

    Two way data binding by ngModel

--- databinding/src/app/twowaybinding/twowaybinding.component.ts
+++ databinding/src/app/twowaybinding/twowaybinding.component.ts
@@ -8,6 +8,7 @@ import {Component, OnInit} from '@angular/core';
 export class TwowaybindingComponent implements OnInit {
 
   username = "jaymin";
+  usernameWithNgModel = "jaymin";
 
   constructor() {
   }
