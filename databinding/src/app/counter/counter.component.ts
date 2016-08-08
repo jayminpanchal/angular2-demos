3b14deb413120de9afaac7b42ccde09e9222db3d
Jaymin <jayminpanchal10@gmail.com>
2016-08-09 01:05:31 +0530

    counter with + - option limit

--- databinding/src/app/counter/counter.component.ts
+++ databinding/src/app/counter/counter.component.ts
@@ -9,15 +9,20 @@ export class CounterComponent implements OnInit {
   @Input() count = 0;
   @Output() countChange = new EventEmitter<number>();
 
+  min = 0;
+  max = 100;
+
   constructor() {
   }
 
   increment() {
+    if (this.count >= this.max) return;
     this.count++;
     this.countChange.emit(this.count);
   }
 
   decrement() {
+    if (this.count <= this.min) return;
     this.count--;
     this.countChange.emit(this.count);
   }
