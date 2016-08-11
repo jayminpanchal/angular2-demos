c007d86e03c7621c0b8178f6d39ccbff1f31bdb7
Jaymin <jayminpanchal10@gmail.com>
2016-08-12 00:15:19 +0530

    ngFor demo

--- directives/src/app/app.component.ts
+++ directives/src/app/app.component.ts
@@ -8,5 +8,9 @@ import {Component} from '@angular/core';
 export class AppComponent {
   title = 'Directives Demo';
   test = false;
-
+  users = [
+    {id: 1, name: 'jaymin', email: 'jaymin@gmail.com'},
+    {id: 2, name: 'abc', email: 'abc@gmail.com'},
+    {id: 3, name: 'xyz', email: 'xyz@gmail.com'}
+  ];
 }
