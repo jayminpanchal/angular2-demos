03f90b37fec11ced053c7aa2d8489abaf6e6284d
Jaymin <jayminpanchal10@gmail.com>
2016-08-15 00:14:18 +0530

    Basic service created with Loggin demo

--- services/src/app/app.component.ts
+++ services/src/app/app.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {LoggingService} from "./logging.service";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers: [LoggingService]
 })
 export class AppComponent {
-  title = 'app works!';
+  title = 'Service Demo!';
+
+  constructor(private logger: LoggingService) {
+  }
+
+  logIt() {
+    this.logger.log();
+  }
 }
