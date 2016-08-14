b31c912eedb15a0d65295c1b30a7cb174dc06f26
Jaymin <jayminpanchal10@gmail.com>
2016-08-15 02:08:09 +0530

    manually imported rxjs map to solve issue

--- services/src/app/httpservice.service.ts
+++ services/src/app/httpservice.service.ts
@@ -1,5 +1,7 @@
 import {Injectable} from '@angular/core';
 import {Http} from "@angular/http";
+import {Observable} from 'rxjs/Rx';
+import 'rxjs/add/operator/map';
 
 @Injectable()
 export class HttpserviceService {
